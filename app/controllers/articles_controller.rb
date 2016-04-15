8610e56b18dae3b73445ba22935dfb717f5d453e
ajithrajan <ajith.ijk@gmail.com>
2016-04-15 07:59:48 -0700

    Updated flash message styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ before_action :set_article, only:[:edit, :update, :show, :destroy]
 		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
 		if @article.save
-			flash[:notice] = "Article was sucessfully created"
+			flash[:success] = "Article was sucessfully created"
 			redirect_to article_path(@article)
 		else
 			render 'new'
@@ -28,7 +28,7 @@ before_action :set_article, only:[:edit, :update, :show, :destroy]
 
 	def update
 		if @article.update(article_params)
-			flash[notice] = "Article was sucessfully updated"
+			flash[:success] = "Article was sucessfully updated"
 			redirect_to article_path(@article)
 		else
 			render 'edit'
@@ -37,7 +37,7 @@ before_action :set_article, only:[:edit, :update, :show, :destroy]
 
 	def destroy
 		@article.destroy
-		flash[:notice] = "Article was successfully deleted"
+		flash[:danger] = "Article was successfully deleted"
 		redirect_to articles_path
 	end
 	
